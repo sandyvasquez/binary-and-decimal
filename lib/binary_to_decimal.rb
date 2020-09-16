4be9e644822a83730d7003d06f9b7bd570ae0a11
Sandy Vasquez <sandyvasquez9@gmail.com>
2020-09-16 10:54:37 -0700

    completed assignment

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -3,7 +3,18 @@
 # The most significant bit is at index 0.
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
-# the algorithm you devised in class.
+# the algorithm you devised in #class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal_value = 0
+  exponent_value = 0
+
+  binary_array.reverse.each do |place|
+    decimal_value += place * 2 ** exponent_value
+    # puts "#{place} * 2 ** #{exponent_value}"
+    exponent_value+= 1
+  end
+  return decimal_value
+  # raise NotImplementedError
 end
+
+# binary_to_decimal([1,0,0,0,0,0,0,1,])
